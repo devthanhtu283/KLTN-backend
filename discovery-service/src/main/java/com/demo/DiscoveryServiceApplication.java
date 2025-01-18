d60d2a74aebb8bf5c2195dcfc46a6ac7685183c2
devthanhtu283 <devthanhtu283@gmail.com>
2025-01-18 10:44:30 +0700

    config microservice

--- discovery-service/src/main/java/com/demo/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/demo/DiscoveryServiceApplication.java
@@ -2,14 +2,14 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class UserServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
--- quiz-service/src/main/java/com/demo/QuizServiceApplication.java
+++ quiz-service/src/main/java/com/demo/QuizServiceApplication.java
@@ -2,12 +2,14 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class JobsApplication {
+@EnableDiscoveryClient
+public class QuizServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobsApplication.class, args);
+		SpringApplication.run(QuizServiceApplication.class, args);
 	}
 
 }
