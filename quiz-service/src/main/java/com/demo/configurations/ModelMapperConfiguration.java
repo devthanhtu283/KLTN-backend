e1df29333d95193762def147c82f3a343e58001c
devthanhtu283 <devthanhtu283@gmail.com>
2025-01-21 22:42:06 +0700

    update

--- quiz-service/src/main/java/com/demo/configurations/ModelMapperConfiguration.java
+++ quiz-service/src/main/java/com/demo/configurations/ModelMapperConfiguration.java
@@ -59,6 +59,7 @@ public class ModelMapperConfiguration {
 			protected void configure() {
 				// TODO Auto-generated method stub
 				map().setUsername(source.getUserID().getUsername());
+				map().setEmail(source.getUserID().getEmail());
 			}
 			
 		});
